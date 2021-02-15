7b28a75386f03b20f0bdbf311281fbc079d2afd4
Lane Harris <lrharris215@gmail.com>
2021-02-15 11:03:03 -0500

    Created Users controller and added helper methods to ApplicationController

---
+++ Reddit_Clone/app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :require_logged_in, only:[:destroy]
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save && log_in(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = ["Invalid username or Password"]
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if @user 
+      render :show
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to users_url
+    end
+  end
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
